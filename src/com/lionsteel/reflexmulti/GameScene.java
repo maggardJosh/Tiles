04401375e5880392bc0ca0887f56ddca962ed3c7
maggardJosh <jmaggard62@gmail.com>
2013-01-25 18:56:46 -0600

    Added intros

--- src/com/lionsteel/reflexmulti/GameScene.java
+++ src/com/lionsteel/reflexmulti/GameScene.java
@@ -4,9 +4,14 @@ import java.util.Random;
 
 import org.andengine.engine.handler.IUpdateHandler;
 import org.andengine.entity.IEntity;
+import org.andengine.entity.modifier.MoveByModifier;
 import org.andengine.entity.modifier.MoveModifier;
+import org.andengine.entity.modifier.MoveXModifier;
+import org.andengine.entity.modifier.ScaleModifier;
+import org.andengine.entity.modifier.SequenceEntityModifier;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.sprite.Sprite;
+import org.andengine.input.touch.TouchEvent;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
 import org.andengine.opengl.texture.region.TextureRegion;
@@ -24,6 +29,12 @@ public class GameScene extends Scene implements ReflexConstants
 
 	private final Sprite		barSprite;
 
+	private final Sprite		playerOneIntro;
+	private final Sprite		playerTwoIntro;
+
+	private boolean				playerOneReady			= false;
+	private boolean				playerTwoReady			= false;
+
 	private GameButton[]		gameButtons				= new GameButton[6];
 
 	public GameScene()
@@ -31,17 +42,59 @@ public class GameScene extends Scene implements ReflexConstants
 		activity = ReflexActivity.getInstance();
 		rand = new Random();
 		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/GameScene/");
-		sceneAtlas = new BitmapTextureAtlas(activity.getTextureManager(), 1024, 512);
+		sceneAtlas = new BitmapTextureAtlas(activity.getTextureManager(), 1024, 2048);
 		final TextureRegion backgroundRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(sceneAtlas, activity, "background.png", 0, 0);
 		final TextureRegion barRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(sceneAtlas, activity, "bar.png", 0, (int) backgroundRegion.getHeight());
+		final TextureRegion playerOneIntroRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(sceneAtlas, activity, "playerOneIntro.png", 0, (int) (barRegion.getTextureY() + barRegion.getHeight()));
+		final TextureRegion playerTwoIntroRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(sceneAtlas, activity, "playerTwoIntro.png", 0, (int) (playerOneIntroRegion.getTextureY() + playerOneIntroRegion.getHeight()));
 		sceneAtlas.load();
 		final Sprite backgroundSprite = new Sprite(0, 0, backgroundRegion, activity.getVertexBufferObjectManager());
 
-		barSprite = new Sprite((CAMERA_WIDTH-barRegion.getWidth())/2,CAMERA_HEIGHT-BAR_HEIGHT,barRegion,activity.getVertexBufferObjectManager());
-		
+		barSprite = new Sprite((CAMERA_WIDTH - barRegion.getWidth()) / 2, CAMERA_HEIGHT - BAR_HEIGHT, barRegion, activity.getVertexBufferObjectManager());
+
+		playerOneIntro = new Sprite(0, 0, playerOneIntroRegion, activity.getVertexBufferObjectManager()){
+			@Override
+			public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float pTouchAreaLocalX, float pTouchAreaLocalY)
+			{
+				if(pSceneTouchEvent.getAction() == TouchEvent.ACTION_DOWN)
+				{
+					playerOneIntro.registerEntityModifier(new MoveXModifier(1.0f, playerOneIntro.getX(), -playerOneIntro.getWidth()){
+						@Override
+						protected void onModifierFinished(IEntity pItem)
+						{
+							playerOneReady = true;
+							super.onModifierFinished(pItem);
+						}
+					});
+				}
+				return super.onAreaTouched(pSceneTouchEvent, pTouchAreaLocalX, pTouchAreaLocalY);
+			}
+		};
+		playerTwoIntro = new Sprite(CAMERA_WIDTH-playerTwoIntroRegion.getWidth(), 0, playerTwoIntroRegion, activity.getVertexBufferObjectManager()){
+			@Override
+			public boolean onAreaTouched(TouchEvent pSceneTouchEvent, float pTouchAreaLocalX, float pTouchAreaLocalY)
+			{
+				if(pSceneTouchEvent.getAction() == TouchEvent.ACTION_DOWN)
+				{
+					playerTwoIntro.registerEntityModifier(new MoveXModifier(1.0f, playerTwoIntro.getX(), CAMERA_WIDTH){
+						@Override
+						protected void onModifierFinished(IEntity pItem)
+						{
+							playerTwoReady = true;
+							super.onModifierFinished(pItem);
+						}
+					});
+				}
+				return super.onAreaTouched(pSceneTouchEvent, pTouchAreaLocalX, pTouchAreaLocalY);
+			}
+		};
+
+		this.registerTouchArea(playerOneIntro);
+		this.registerTouchArea(playerTwoIntro);
 		this.attachChild(backgroundSprite);
 		this.attachChild(barSprite);
 		
+
 		for (int x = 0; x < 6; x++)
 		{
 			GameButton button = new GameButton(x + 1, this, PLAYER_ONE);
@@ -79,6 +132,9 @@ public class GameScene extends Scene implements ReflexConstants
 
 			}
 		});
+		
+		this.attachChild(playerOneIntro);
+		this.attachChild(playerTwoIntro);
 
 	}
 
@@ -97,13 +153,17 @@ public class GameScene extends Scene implements ReflexConstants
 						pItem.setPosition((CAMERA_WIDTH - BUTTON_WIDTH) / 2, (CAMERA_HEIGHT - BUTTON_WIDTH) / 2);
 						pItem.setVisible(false);
 						changeState(GameState.PICKING_NEW_BUTTON);
-						switch(button.getPlayer())
+						switch (button.getPlayer())
 						{
 						case PLAYER_ONE:
-							barSprite.setX(barSprite.getX()-10);
+							barSprite.registerEntityModifier(new MoveByModifier(.4f, -BAR_SPEED, 0));
+							barSprite.registerEntityModifier(new SequenceEntityModifier(new ScaleModifier(.2f, 1.0f, 1.0f, barSprite.getScaleY(), 1.5f), new ScaleModifier(.2f, 1.0f, 1.0f, 1.5f, 1.0f)));
+							//barSprite.setX(barSprite.getX()-10);
 							break;
 						case PLAYER_TWO:
-							barSprite.setX(barSprite.getX()+10);
+							//barSprite.setX(barSprite.getX()+10);
+							barSprite.registerEntityModifier(new MoveByModifier(.4f, BAR_SPEED, 0));
+							barSprite.registerEntityModifier(new SequenceEntityModifier(new ScaleModifier(.2f, 1.0f, 1.0f, barSprite.getScaleY(), 1.5f), new ScaleModifier(.2f, 1.0f, 1.0f, 1.5f, 1.0f)));
 							break;
 						}
 						super.onModifierFinished(pItem);
@@ -119,7 +179,7 @@ public class GameScene extends Scene implements ReflexConstants
 		switch (gameState)
 		{
 		case GameState.INTRO:
-			if (secondsOnCurrentState >= 1)
+			if(playerOneReady&&playerTwoReady)
 				changeState(GameState.PICKING_NEW_BUTTON);
 			break;
 		case GameState.PICKING_NEW_BUTTON:
--- src/com/lionsteel/reflexmulti/ReflexConstants.java
+++ src/com/lionsteel/reflexmulti/ReflexConstants.java
@@ -7,7 +7,8 @@ public interface ReflexConstants
 
 	public static final int	BUTTON_WIDTH	= 150;
 	public static final int	BAR_HEIGHT		= 30;
-
+	public static final int BAR_SPEED = 30;
+	
 	public static final int	PLAYER_ONE		= 0;
 	public static final int	PLAYER_TWO		= 1;
 }
