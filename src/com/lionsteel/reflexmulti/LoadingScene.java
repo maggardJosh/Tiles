20651ee1d99d27630e55edcc404462939b40c08a
maggardJosh <jmaggard62@gmail.com>
2013-02-02 23:15:20 -0600

    Black Background on Loading Scene Semi-transparent background added to loading scene

--- src/com/lionsteel/reflexmulti/LoadingScene.java
+++ src/com/lionsteel/reflexmulti/LoadingScene.java
@@ -1,5 +1,6 @@
 package com.lionsteel.reflexmulti;
 
+import org.andengine.entity.primitive.Rectangle;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.sprite.Sprite;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
@@ -22,10 +23,13 @@ public class LoadingScene extends Scene implements ReflexConstants
 		
 		sceneAtlas.load();
 		
+		Rectangle background = new Rectangle(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT, activity.getVertexBufferObjectManager());
+		background.setColor(0,0,0,.9f);
 		//setBackground(new Background(0, 0, 0, .4f));
 		setBackgroundEnabled(false);
 		final Sprite loadingSprite = new Sprite((CAMERA_WIDTH - loadingRegion.getWidth()) / 2, (CAMERA_HEIGHT - loadingRegion.getHeight()) / 2, loadingRegion, activity.getVertexBufferObjectManager());
 		
+		this.attachChild(background);
 		this.attachChild(loadingSprite);
 		
 	}
